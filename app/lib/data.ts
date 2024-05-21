4e9610c775d3022b580a3230436500dc91c9b2f5
Joaquin Carbonell <joaquin.carbonell@babelgroup.com>
2024-05-21 09:46:40 +0200

    add cache back

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,12 +9,12 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-// import { unstable_noStore as noStore } from 'next/cache';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  //noStore();
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
@@ -36,7 +36,7 @@ export async function fetchRevenue() {
 export async function fetchRevenueSlow() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  // noStore();
+  //noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
@@ -56,7 +56,7 @@ export async function fetchRevenueSlow() {
 }
 
 export async function fetchLatestInvoices() {
-  // noStore();
+  noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -77,7 +77,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  // noStore();
+  noStore();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -118,7 +118,7 @@ export async function fetchFilteredInvoices(
   currentPage: number,
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-  // noStore();
+  noStore();
   try {
     const invoices = await sql<InvoicesTable>`
       SELECT
