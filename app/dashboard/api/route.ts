ea90f2cabaed5556ee829cd66e16c5121e557d83
Joaquin Carbonell <joaquin.carbonell@babelgroup.com>
2024-05-22 17:06:05 +0200

    add fake data

--- app/dashboard/api/route.ts
+++ app/dashboard/api/route.ts
@@ -1,8 +1,19 @@
-import {
-  fetchRevenue,
-} from '@/app/lib/data';
+
 export async function GET(request: Request) {
-  const data = await fetchRevenue();
+  const data = [
+    { month: 'Jan', revenue: 2000 },
+    { month: 'Feb', revenue: 1800 },
+    { month: 'Mar', revenue: 2200 },
+    { month: 'Apr', revenue: 2500 },
+    { month: 'May', revenue: 2300 },
+    { month: 'Jun', revenue: 3200 },
+    { month: 'Jul', revenue: 3500 },
+    { month: 'Aug', revenue: 3700 },
+    { month: 'Sep', revenue: 2500 },
+    { month: 'Oct', revenue: 2800 },
+    { month: 'Nov', revenue: 3000 },
+    { month: 'Dec', revenue: 4800 },
+  ];
 
   return Response.json(data);
 }
